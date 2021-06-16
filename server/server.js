d9b160890fed42ed13320f9f3ef032bf467bc968
Big Head <spencer.moyer16@yahoo.com>
2021-06-16 15:17:34 -0700

    added queries to components for rendering the query data to pages

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ app.use(express.json());
 
 // Serve up statis assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.statis(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
 app.get('*', (req, res) => {
